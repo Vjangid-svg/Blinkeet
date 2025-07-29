ce6df0137a1e8eeae7bc617136234b09a42c4bfc
Vishnu Jangid <your-email@example.com>
2025-07-29 13:34:50 +0530

    Your commit message here

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -8,7 +8,7 @@ import image1 from "../assets/minute_delivery.png";
 import image2 from "../assets/Best_Prices_Offers.png";
 import image3 from "../assets/Wide_Assortment.png";
 import { DisplayPriceInRupees } from "../utils/DisplayPriceInRupees";
-import { pricewithDiscount } from "../../../../cblink/main/client/src/utils/PriceWithDiscount";
+import { pricewithDiscount } from "../utils/PriceWithDiscount";
 import Divider from "../components/Divider";
 import AddToCartButton from "../components/AddToCartButton";
 
