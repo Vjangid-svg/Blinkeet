c12db11151290808c680f98ebbf08a549bde2c83
Vishnu <vishnujangid7413666@gmail.com>
2025-07-29 13:40:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,12 +17,21 @@ import orderRouter from "./routes/order.route.js";
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blinkeet.vercel.app",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan());
